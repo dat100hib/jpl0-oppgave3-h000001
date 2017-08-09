7ffce9018822683f16b5b4080e49aefb0b3a0d85
wruiwr <wruiwr86@gmail.com>
2017-08-09 18:55:24 +0200

    first commit

---
+++ src/main/java/no/hvl/dat100/Kalkulator.java
@@ -0,0 +1,20 @@
+package main.java.no.hvl.dat100;
+
+public class Kalkulator {
+
+	public static int add(int a, int b) {
+		return a+b;
+	}
+
+	public static int sub(int a, int b) {
+		throw new RuntimeException("sub ikke implementert");
+	}
+
+	public static int mul(int a, int b) {
+		throw new RuntimeException("mul ikke implementert");
+	}
+
+	public static int div(int a, int b) {
+		throw new RuntimeException("div ikke implementert");
+	}
+}
